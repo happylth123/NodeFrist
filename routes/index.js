dad48065a3b7c8d73912e2753083512b2cede7d1
tenghui <1246623393@qq.com>
2017-09-15 23:26:38 +0800

    ~test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My First Node App' });
+  res.render('index', { title: 'My First NodeJS App' });
 });
 
 module.exports = router;
